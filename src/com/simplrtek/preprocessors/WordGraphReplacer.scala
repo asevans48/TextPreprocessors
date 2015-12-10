a5b89bcaabd726e9faa06c1d4c73a6f6e53a48e5
asevans48 <aevans@simplrterms.com>
2015-12-10 00:00:06 -0700

    More updates

--- src/com/simplrtek/preprocessors/WordGraphReplacer.scala
+++ src/com/simplrtek/preprocessors/WordGraphReplacer.scala
@@ -5,31 +5,87 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import java.util.concurrent.ConcurrentHashMap
 
+import breeze.linalg.DenseMatrix
+import breeze.linalg.DenseVector
+
+import sbt.io.IO
+import java.io.File
+
 /**
- * Replace words in an object and create a replacement model.
+ * Replace words in an object and create a replacement model. This is not distributable because of the iterative nature of building a replacement model.
+ * However, concurrency is used where possible.
  */
 object WordGraphReplacer {
-  
+  var tokenizer:List[String] = List[String]() //for tokenizing a sentence to a vector (must be built first)
   var replacementMap:ConcurrentHashMap[String,String] = new ConcurrentHashMap[String,String]()
+  
+  /**
+   * The first step in the algorithm. Builds a tokenizer to create vectors from sentences.
+   * 
+   * @param		directory		The directory to use in building the tokenizer.
+   */
+  def buildTokenizer(directory:File)={
+    
+  }
   
   /**
    * Take in a string and perform replacement.
    * 
    * @param		text									The string to replace words from
+   * @param		cosCutoff							The cosine cutoff to consider in whether to replace a word (based on best definition)
    * @param		{String}{binFile}			The model to use in sentence tokenization
    * @param		{Duration}{termTime]	The durationt to wait for replacement to finish.
    * @return	A List of List of words representing sentences void of punctuation
    */
-  def replaceWords(text:String,binFile:String = "data/models/en-token.bin",termTime:Duration = Duration.Inf)={
+  def replaceWords(text:String,cosCutoff:Double = .9,binFile:String = "data/models/en-token.bin",termTime:Duration = Duration.Inf):List[List[String]]={
+    
+    if(tokenizer.size == 0){
+      try{
+        throw new NullPointerException("This is a two pass algorithm. Tokenizer must be generated first")
+      }catch{
+        case e:NullPointerException => println(e.getMessage+" "+e.getStackTraceString)
+        sys.exit()
+      }
+    }
     
     def replaceWords(sentence:String):Future[List[String]]=Future{
+      
+      def getDefinition(words:List[String],word:String):List[String]={
+        null
+      }//get the definition using cosines
+      
+      def checkDefs()={
+        
+      }
+      
       var words = WordTokenizer.wordTokenize(PunctReplacer.replacePunct(text))
-      words.foreach { x => ??? }
+      words = words.filter({ word => !StopWords.stopList.contains(word) })
+      
+      
       null
     }
     
     SentTokenizer.load(binFile)
     Await.result(Future.traverse(SentTokenizer.getSentences(text).toList)(replaceWords(_)),termTime)
+  }
+  
+  
+  /**
+   * The function that performs the actual
+   *
+   * @param		directory		The directory where the files reside.
+   * @return	A list of list of strings representing sentences with certain words replaced.
+   */
+  def replace(directory:File):List[List[String]]={
+    var sentences:List[List[String]] = List[List[String]]()
+    buildTokenizer(directory)
+    IO.listFiles(directory).foreach { 
+      file =>
+        var text = IO.read(file)
+        sentences = sentences ++ replaceWords(text)
+     }
+    sentences
+    
   }
   
 }
