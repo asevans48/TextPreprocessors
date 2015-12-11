2044c0aa0d9da1cd7f69f92937a4dc7cdb1f36a2
asevans48 <droidinator3000@gmail.com>
2015-12-11 10:20:35 -0700

    Pickling set up as part of an external Object using Java Object Streams.

---
+++ src/com/simplrtek/pickle/Pickler.scala
@@ -0,0 +1,78 @@
+package com.simplrtek.pickle
+
+import java.io.{ObjectInputStream,ObjectOutputStream}
+import java.io.{File,FileOutputStream,FileInputStream}
+import java.io.{IOException,FileNotFoundException}
+
+import com.simplrtek.preprocessors.logger
+
+/**
+ * Pickles objects (which is the process) using Java Object Array Streams to maintain compatibility with
+ * Scala 2.10.5+ versions of Scala 2.10.X. Paradise is missing the specific macros
+ * for anything over Scala 2.10.4. Mahout Scala bindings require Scala 2.10.X.
+ * 
+ * The implementation is binary by default. A faster library will be used when Mahout bindings
+ * are updated to 2.11.X + 
+ *
+ * Primitives and basic objects can be pickled as is but classes and objects require extending Serializable.
+ * The annotation is deprecated. 
+ */
+object Pickler {
+  
+  protected var log = logger.getLogger()
+  
+  /**
+   * Pickle an object to a file.
+   * 
+   * @param		picklee		The object of type [T] to pickle.
+   * @param		file			The file to pickle to.
+   */
+  def pickleTo[T](picklee:T,file:File)={
+    try{
+      val foos = new FileOutputStream(file)
+      val oos = new ObjectOutputStream(foos)
+      oos.writeObject(picklee)
+      oos.close()
+      foos.close()
+    }catch{
+      case e:IOException =>{
+        log.info("IO Exception in Writing Pickle: "+e.getMessage+"\n"+e.getStackTraceString)
+      }
+      case e:NullPointerException =>{
+        log.info("Picklee object or file are Null: "+e.getMessage+"\n"+e.getStackTraceString)
+      }
+      case t:Throwable =>{
+        log.info("Error in Pickling: "+t.getMessage+"\n"+t.getStackTraceString)
+      }
+    }
+  }
+  
+  /**
+   * Unpickle a binary object from a file.
+   * 
+   * @param		file		The file to unpickle from.
+   * @return	The object of type [T] from the file.
+   */
+  def unpickle[T](file:File):T={
+    try{
+      val fois:FileInputStream = new FileInputStream(file)
+      val ois:ObjectInputStream = new ObjectInputStream(fois)
+      ois.readObject().asInstanceOf[T]
+    }catch{
+       case e:IOException =>{
+        log.info("IO Exception in UnPickling from file: "+e.getMessage+"\n"+e.getStackTraceString)
+        sys.exit()
+      }
+      case e:NullPointerException =>{
+        log.info("Unpickle File is Null: "+e.getMessage+"\n"+e.getStackTraceString)
+        sys.exit()
+      }
+      case t:Throwable =>{
+        log.info("Error in UnPickling from File: "+t.getMessage+"\n"+t.getStackTraceString)
+        sys.exit()
+      }
+      
+    }
+  }
+  
+}
