efaf0367322ffa4a4e8fd21fbacfd839011de0f1
asevans48 <aevans@simplrterms.com>
2015-12-15 23:31:01 -0700

    need to refresh windows. massive issue. checking

--- src/com/simplrtek/hashing/FeatureHasher.scala
+++ src/com/simplrtek/hashing/FeatureHasher.scala
@@ -1,21 +1,62 @@
 package com.simplrtek.hashing
 
 import com.simplrtek.tokenizers.CountTokenizer
-
+import breeze.linalg.{DenseVector,DenseMatrix}
+import breeze.linalg.{CSCMatrix,SparseVector}
 /**
  * https://github.com/scikit-learn/scikit-learn/blob/c9572494a82b364529374aafca15660a7366e2c4/sklearn/feature_extraction/_hashing.pyx
  * https://github.com/scikit-learn/scikit-learn/blob/c9572494a82b364529374aafca15660a7366e2c4/sklearn/utils/murmurhash.pyx
  * 
  * Hashes Text Features to matrices.
  */
-class FeatureHasher(features:Double = math.pow(2, 20)){
-  var nfeatures = features
+class FeatureHasher{
   
   /**
-   * Converts a List of String,Count Pairs to a Dense Matrix
+   * If there are more than 10000 features a resize is necessary.
+   */
+  def resizeVector(arr:Array[Integer],size:Integer):Array[Integer]={
+    var newArr = Array.fill[Integer][size](0)
+    Array.copy(arr, 0, newArr, 0, arr.length)
+  }
+  
+  /**
+   * Converts a List of String,Count Pairs to a CSC Matrix
    * using the hashing trick.
    */
-  def transform(counts:List[Map[String,Integer]])={
+  def transform(counts:List[Map[String,Integer]],features:Integer = 10000):CSCMatrix[Integer]={
+    val builder = new CSCMatrix.Builder[Double](rows=10, cols=10)
+    
+    var size:Integer = 0
+    var indexList:List[Array[Integer]] = List[Array[Integer]]()
+    var datList:List[Array[Integer]] = List[Array[Integer]]()
+    for(sentence <- counts){
+      var indices:Array[Integer]= Array[Integer]()
+      var values:Array[Integer] = Array.fill[Integer][features](0) 
+      
+      for(key <- sentence.keys){
+        val v = sentence.get(key).get
+        val h = Hash.murmurHashString(key)
+        
+        if(indices.length > features){
+          indices = resizeVector(indices,indices.length + 1)
+        }
+        
+        indices(indices.length -1) = Math.abs(h) % features
+        v = v * ((h*2) -1)
+        values(size) = v
+        
+        size += 1
+        
+        if(size > values.length){
+          values=resizeVector(values,(values.length *2))
+        }
+        
+      }
+      
+      indexList = indexList :+ indices
+      datList = datList :+ values
+    }
+    //create a dense matrix from the list of arrays
     
   }
 }
--- src/com/simplrtek/hashing/Hash.scala
+++ src/com/simplrtek/hashing/Hash.scala
@@ -1,15 +1,47 @@
 package com.simplrtek.hashing
+
+import scala.util.hashing.MurmurHash3
+import com.google.common.hash.Murmur3_32HashFunction
+
 /**
  * Several hashing algorithms for such tasks as the hashing trick.
  */
 object Hash {
   
-  def hornersHash()={
-    
+  /**
+   * Use Horners Hash to hash a string.
+   * 
+   * @param		text		The string to hash
+   * @return	The signed integer from the hash.
+   */
+  def hornersHash(text:String,prime:Integer = 31):Long={
+    var hash:Long = 0L
+    for(i <- 0 to text.size){
+      hash += (prime * hash) + text.charAt(i)
+    }
+    hash
   }
   
-  def singleBitHash()={
-    
+  /**
+   * Use murmurHash3 to hash a byte array.
+   * 
+   * @param		data		The byte array to hash
+   * @return	Returns the hashed integer.
+   */
+  def murmurHash(data:Array[Byte]):Integer={
+    //Murmur3_32HashFunction
+    MurmurHash3.bytesHash(data)  
+  }
+  
+  /**
+   * Use murmurHash3 to create a 32 bit based integer hash from a string and 
+   * return it as a long.
+   * 
+   * @param		text		The string to hash
+   * @return	The hash Integer as a long.
+   */
+  def murmurHashString(text:String):Integer={
+    MurmurHash3.stringHash(text)
   }
   
 }
