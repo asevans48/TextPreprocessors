72b208b665b511e4007e4c636e5dcbbac9392f90
asevans48 <aevans@simplrterms.com>
2016-02-23 00:46:32 -0700

    Sparse Vector Slicing aids

--- src/com/simplrtek/enriched/breeze/EnrichedCSCMatrix.scala
+++ src/com/simplrtek/enriched/breeze/EnrichedCSCMatrix.scala
@@ -1,12 +1,91 @@
 package com.simplrtek.enriched.breeze
 
-import breeze.linalg.CSCMatrix
+import breeze.linalg.{CSCMatrix,SparseVector}
 import scala.concurrent.{Await,Future}
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
+import org.apache.commons.lang3.exception.ExceptionUtils
+import scala.collection.mutable.ArrayBuffer
 
 class EnrichedCSCMatrix(matrix : CSCMatrix[Double]) {
-
+  
+  /**
+   * Slice a series of columns from a CSC Matrix. Start and end columns are zero based.
+   * If the indices for the columns are appropriate, returns a copy of these columns
+   * no matter what.
+   * 
+   * @param			start			The starting column (included)
+   * @param			end 			The final column (included). 
+   * @return		CSCMatrix			A new CSCMatrix of the shape [matrix.rows,start-end columns]
+   */
+  def sliceCols(start : Int, end : Int):CSCMatrix[Double]={
+    if(start < 0 || end > matrix.rows){
+        try{
+          throw new Exception(s"Indices must be within column $start and column $end") 
+        }catch{
+          case t : Throwable => println(t.getMessage+"\n"+ExceptionUtils.getStackTrace(t))
+        }
+    }
+    
+    var csc = new CSCMatrix.Builder[Double](rows = matrix.rows,cols = end - start)
+    
+    
+    for(i <- start to end){
+      var start = matrix.colPtrs.apply(i)
+      var end = if(i + 1 < matrix.cols) matrix.colPtrs(i+1) else matrix.data.length
+      
+      while(start < end){
+        csc.add( matrix.rowIndices(start),i, matrix.data(start))
+        start += 1
+      }
+      
+    }
+    
+    csc.result
+  }
+  
+  
+  
+  /**
+   * Get a row from the matrix and return it as a Sparse Vector
+   * 
+   * @param			row 		A row from the matrix
+   * @return		A sparse vector of the data from the column as a Sparse Vector of matrix.rows in size.
+   */
+  def getRow(row : Int):SparseVector[Double]={
+    var data : ArrayBuffer[Double] = new ArrayBuffer[Double]()
+    var indices : ArrayBuffer[Int] = new ArrayBuffer[Int]()
+    
+    for(col <- 0 to matrix.cols){
+      if(matrix.apply(row,col) != 0.0){
+        data += matrix.apply(row, col)
+        indices += col
+      }
+    }
+    
+    new SparseVector(indices.toArray,data.toArray,matrix.cols)
+  }
+  
+  /**
+   * Slice  a column from the matrix and return a Sparse Vector
+   * 
+   * @param		col			The column to slice from
+   * @return	A sparse vector of data from the matrix of matrix.rows in size
+   */
+  def getColumn(col : Int):SparseVector[Double]={
+    var data : ArrayBuffer[Double] = new ArrayBuffer[Double]()
+    var indices : ArrayBuffer[Int] = new ArrayBuffer[Int]()
+    
+    for(row <- 0 to matrix.rows){
+      if(matrix.apply(row,col) != 0.0){
+        data += matrix.apply(row, col)
+        indices += col
+      }
+    }
+    
+    new SparseVector(indices.toArray,data.toArray,matrix.cols)
+  }
+  
   
   def getNNZ():Int={
     matrix.activeSize
